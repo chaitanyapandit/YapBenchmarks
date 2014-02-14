d1ed436c78c4064910b9aa1e04a8bd6421761c43
Chaitanya <chaitanyapandit@me.com>
2014-02-14 14:24:14 +0530

    Setup Cocoapods. Added CoreData model. Restructured

---
+++ YapBenchmarks/Classes/ITAppDelegate.h
@@ -0,0 +1,15 @@
+//
+//  ITAppDelegate.h
+//  YapBenchmarks
+//
+//  Created by Chaitanya Pandit on 14/02/14.
+//  Copyright (c) 2014 #include tech. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface ITAppDelegate : UIResponder <UIApplicationDelegate>
+
+@property (strong, nonatomic) UIWindow *window;
+
+@end
---
+++ YapBenchmarks/Classes/ITSecondViewController.h
@@ -0,0 +1,13 @@
+//
+//  ITSecondViewController.h
+//  YapBenchmarks
+//
+//  Created by Chaitanya Pandit on 14/02/14.
+//  Copyright (c) 2014 #include tech. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface ITSecondViewController : UIViewController
+
+@end
