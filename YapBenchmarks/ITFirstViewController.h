72dc057a5d4ea88117945bcd0799be11784a20b0
Chaitanya <chaitanyapandit@me.com>
2014-02-14 13:04:48 +0530

    Initial checkin

---
+++ YapBenchmarks/ITFirstViewController.h
@@ -0,0 +1,13 @@
+//
+//  ITFirstViewController.h
+//  YapBenchmarks
+//
+//  Created by Chaitanya Pandit on 14/02/14.
+//  Copyright (c) 2014 #include tech. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface ITFirstViewController : UIViewController
+
+@end
---
+++ YapBenchmarks/main.m
@@ -0,0 +1,18 @@
+//
+//  main.m
+//  YapBenchmarks
+//
+//  Created by Chaitanya Pandit on 14/02/14.
+//  Copyright (c) 2014 #include tech. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+#import "ITAppDelegate.h"
+
+int main(int argc, char * argv[])
+{
+    @autoreleasepool {
+        return UIApplicationMain(argc, argv, nil, NSStringFromClass([ITAppDelegate class]));
+    }
+}
